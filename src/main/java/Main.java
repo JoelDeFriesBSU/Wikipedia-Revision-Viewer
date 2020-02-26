6905b781d453e65c5a114d214042b107093d0a5d
JoelDeFriesBSU <jtdefries@bsu.edu>
2020-02-26 12:09:06 -0500

    more initial files

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import com.google.gson.JsonArray;
+import exceptions.StringIsNotJsonException;
+import json_utils.WikiArticleExporter;
+import json_utils.WikiArticleParser;
+import wikipedia.WikiArticleEdits;
+
+public class Main {
+
+    public static void main(String[] args) throws StringIsNotJsonException {
+
+        while(true){
+            System.out.println("Type a Wiki article (press 'quitprogram' to quit):");
+            Scanner s = new Scanner(System.in);
+            String word = s.nextLine();
+
+            if(word.equals("quitprogram")){
+                System.out.println("Quitting...");
+                break;
+            }else{
+                String wordJson = WikiArticleParser.parseWikiArticleToJson(word);
+                System.out.println(wordJson);
+                JsonArray articleEdits = WikiArticleExporter.exportArticleEditInfo(wordJson);
+
+                System.out.println(articleEdits);
+            }
+        }
+    }
+}
